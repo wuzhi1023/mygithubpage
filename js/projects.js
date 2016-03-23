38612de449cb614c1a2a951e9ffadd1101c19ba5
wuzhi1023 <woozhh@gmail.com>
2016-03-23 00:30:50 -0600

    version 0.8

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,18 @@
 $(function() {
-  $('.project').click(function() {
-    href = $(this).attr("goto");
-    console.log(href);
-    $('#transition-block').velocity({top:0},{complete: function(){window.location=href;}});
-  });
+    var height = $('.hover-block').first().width();
+    $('.hover-block').css('height', height);
+    $('.project').click(function() {
+        href = $(this).attr("goto");
+        console.log(href);
+        $('div:not(#transition-block)').velocity({opacity:0},800);
+        $('#transition-block').velocity({top:0},{
+        	queue: false,
+        	duration: 800,
+        	easing: 'ease-in',
+        	complete: function(){window.location=href;}});
+    });
+    $( window ).resize(function() {
+        var height = $('.hover-block').first().width();
+        $('.hover-block').css('height', height);
+    });
 });
