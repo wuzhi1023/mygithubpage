17b0fac51b9a9b0c983a7f393d230b40b86fd39e
wuzhi1023 <woozhh@gmail.com>
2016-03-24 01:45:43 -0600

    IE fix 3

--- js/profile.js
+++ js/profile.js
@@ -177,4 +177,15 @@ $(function() {
     $('#page-nav-block').css('height', $(newSec).height());
     toggle();
   });
+  if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+    $('body').on("mousewheel", function () {
+        // remove default behavior
+        event.preventDefault(); 
+
+        //scroll without smoothing
+        var wheelDelta = event.wheelDelta;
+        var currentScrollPosition = window.pageYOffset;
+        window.scrollTo(0, currentScrollPosition - wheelDelta);
+    });
+  }
 });
