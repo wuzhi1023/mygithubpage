e8893e20dd2be9f1e93c8693ca4ad2e79b99f3ee
wuzhi1023 <woozhh@gmail.com>
2016-03-26 05:23:10 -0600

    projects animation

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,14 @@ $(function() {
     var height = $('.hover-block').first().width();
     $('.hover-block').css('height', height);
     $('.project').css('height', height);
+
+    $('.project').each(function() {
+        delay = $(this).position().top*0.8 + $(this).position().left;
+        $(this).velocity({backgroundColor: ['#82fffc','#f70'], backgroundColorAlpha: [0.2,1]}, {duration: 50, delay: delay});
+        $(this).children("img").velocity({opacity: 1}, {delay: delay+80});
+        $(this).children(".project-name").velocity({opacity: 1}, {delay: delay+80});
+    });
+
     $('.project').click(function() {
         href = $(this).attr("goto");
         console.log(href);
