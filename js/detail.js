12ce276f0ece56f6a98272a66280c07e6f473934
wuzhi1023 <woozhh@gmail.com>
2016-03-26 17:57:37 -0600

    background color fix

--- js/detail.js
+++ js/detail.js
@@ -7,19 +7,12 @@ $(function() {
 		fixnav = true;
 		$('#fix-navbar').velocity({opacity: 0.9}, 300);
 	}
-	if ($(window).scrollTop() == 0){
-		$('#hero-image').css('top',1000);
-		$('#hero-block').velocity({top: 0}, 600);
-		$('#content-block').velocity({top: 400}, {easing: 'easeOut', duration: 600,delay:100});
-		$('#hero-image').velocity({top: 0, opacity: 1}, {duration: 1000,delay:200, easing: [.3,1.03,.74,1.13],complete: function(){$('#hero-image').css('position','fixed');$('body').css('background-color','#F70');}});
-		$('#shadow').velocity({top: 0}, {duration: 600, delay:200});
-	} else {
-		$('#hero-image').css('top',1000);
-		$('#hero-block').css('top',0);
-		$('#content-block').velocity({top: 400}, {easing: 'easeOut', duration: 600,delay:100});
-		$('#hero-image').velocity({top: 0, opacity: 1}, {duration: 1000,delay:200, easing: [.3,1.03,.74,1.13],complete: function(){$('#hero-image').css('position','fixed');$('body').css('background-color','#F70');}});
-		$('#shadow').velocity({top: 0}, {duration: 600, delay:200});
-	}
+
+	$('#hero-image').css('top',1000);
+	$('#hero-block').velocity({top: 0}, 600);
+	$('#content-block').velocity({top: 400}, {easing: 'easeOut', duration: 600,delay:100});
+	$('#hero-image').velocity({top: 0, opacity: 1}, {duration: 1000,delay:200, easing: [.3,1.03,.74,1.13],complete: function(){$('#hero-image').css('position','fixed');$('body').css('background-color','#ECCEB4');}});
+	$('#shadow').velocity({top: 0}, {duration: 600, delay:200});
 
 	$('#button-bar a').click(function(event) {
 		event.preventDefault();
